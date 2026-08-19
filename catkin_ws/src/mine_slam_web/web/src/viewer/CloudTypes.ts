c6de3d94d5ea17dce204095f4c377807c7e5d6d6
JBaien <115764836+JBaien@users.noreply.github.com>
2026-08-19 20:47:03 +0800

    feat(web): expose v3 scan topology fields

--- catkin_ws/src/mine_slam_web/web/src/viewer/CloudTypes.ts
+++ catkin_ws/src/mine_slam_web/web/src/viewer/CloudTypes.ts
@@ -7,10 +7,14 @@ export interface ParsedCloud {
   positions: Float32Array;
   intensities: Float32Array;
   times: Float32Array;
+  azimuths: Float32Array;
+  ranges: Float32Array;
   rings: Uint16Array;
   lidarIds: Uint8Array;
   classIds: Uint8Array;
   hasLidarId: boolean;
   hasRing: boolean;
   hasTime: boolean;
+  hasAzimuth: boolean;
+  hasRange: boolean;
 }
